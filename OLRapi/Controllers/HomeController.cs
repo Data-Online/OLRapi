ed44c588b785fbd3cc81714c629ab0d1bda45802
Data-Online <graeme@dataonline.co.nz>
2017-06-27 14:51:59 +1200

    Target host update

--- OLRapi/Controllers/HomeController.cs
+++ OLRapi/Controllers/HomeController.cs
@@ -11,7 +11,9 @@ namespace OLRapi.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+            //var zz = Request.Url.GetLeftPart(UriPartial.Authority) + Url.Content("~");
 
+            ViewBag.EventUID = "992fdb3b-7715-4c41-a591-1558899fc91a";
             return View();
         }
     }
